b7e5ce920b760b2fab95987dc63232ab453a5dde
parmjit <engg.parmjit@gmail.com>
2020-06-21 08:35:13 +1000

    adding ,loading in FilmView Controller in first attempt

---
+++ Bollywood Films/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Bollywood Films
+//
+//  Created by PARMJIT SINGH KHATTRA on 21/6/20.
+//  Copyright © 2020 PARMJIT SINGH KHATTRA. All rights reserved.
+//
+
+import Foundation
+struct Constant {
+    let filmCell = "filmCell"
+    let actorCell = "actorCell"
+    let goToActor = "goToActor"
+    
+}
