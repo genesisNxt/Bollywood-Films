8178c86279c86e35b764770f1b084fa487102038
parmjit <engg.parmjit@gmail.com>
2020-06-21 13:38:28 +1000

    second(actor)ViewController working saving, loading also in first attempt

--- Bollywood Films/Controller/ActorTableViewController.swift
+++ Bollywood Films/Controller/ActorTableViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
-
+import CoreData
 class ActorTableViewController: UITableViewController {
 
+    var actor = [Actor]()
+    let constant = Constant()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadActor()
     }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return actor.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: constant.actorCell, for: indexPath)
+        cell.textLabel?.text = actor[indexPath.row].name
+        return cell
+    }
+    // MARK:- add save Functions and methods
     
-
+    @IBAction func addActor(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Actor Name", message: "Bollywood Actor", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ADD", style: .default) { (action) in
+            let newActor = Actor(context: self.context)
+            newActor.name = textField.text
+            newActor.done = false
+            self.actor.append(newActor)
+            self.saveActor()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "new actor"
+            textField = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
+    }
+        func saveActor(){
+        do {
+            try context.save()
+        } catch  {
+            print("Error saving\(error)")
+        }
+        tableView.reloadData()
 }
+    // MARK:- add load Functions
+            func loadActor(){
+                let request: NSFetchRequest<Actor> = Actor.fetchRequest()
+                do {
+                   actor = try context.fetch(request)
+                } catch  {
+                    print("Error Loading\(error)")
+                }
+                
+            }
+}
+
