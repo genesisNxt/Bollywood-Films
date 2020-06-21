b2bfea6139546e887666e8debf467716683286e0
parmjit <engg.parmjit@gmail.com>
2020-06-21 13:19:40 +1000

    add new viewcontroller

---
+++ Bollywood Films/Controller/ActorTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ActorTableViewController.swift
+//  Bollywood Films
+//
+//  Created by PARMJIT SINGH KHATTRA on 21/6/20.
+//  Copyright © 2020 PARMJIT SINGH KHATTRA. All rights reserved.
+//
+
+import UIKit
+
+class ActorTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
